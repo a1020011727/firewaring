bf4247b19cbd12fec3d4d41a896742e404f424da
a1020011727 <1020011727@qq.com>
2020-02-27 11:51:35 +0800

    original

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new myApp());
+
+class myApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "FireWarning",
+      theme: ,
+    );
+  }
+}
